773f396960da4512c288744018d3342155ba177f
xibbar <xibbar@gmail.com>
2012-06-20 10:48:05 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bench::Application.config.secret_token = '1efa7cc784153875323a4df68c3d45ff39ff42b105a085a7aa45d89a7bbf161b8a5058cfd96e70f9e33ee425461dea8a1f3a1dd41eb57a6696dc6444d248fd6d'
